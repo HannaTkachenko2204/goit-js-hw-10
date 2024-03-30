8a57ce37c3244f1d05025f8388ef2a1c6c471d04
Анна <anna.kuvakina91@gmail.com>
2024-03-30 20:26:52 +0200

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,9 +1,13 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import { createLogger } from "vite";
 
 console.log("Timer");
 
 const inputEl = document.querySelector("#datetime-picker");
+const btnEl = document.querySelector("button");
+
+btnEl.disabled = true;
 
 let userSelectedDate;
 
@@ -13,11 +17,24 @@ const options = {
     defaultDate: new Date(), // String null Устанавливает начальную выбранную дату(ы).
     minuteIncrement: 1, // Integer(целое число) 5 Регулирует шаг ввода минут (включая прокрутку)
     onClose(selectedDates) { // Function null Функции, которые будут активироваться каждый раз при закрытии календаря.
-      console.log(selectedDates[0]); // selectedDates — массив объектов Date, выбранных пользователем. Если даты не выбраны, массив пуст.
+    // selectedDates — массив объектов Date, выбранных пользователем. Если даты не выбраны, массив пуст.
+      if(selectedDates[0].getTime() < options.defaultDate.getTime()) {
+        window.alert("Please choose a date in the future");
+        btnEl.disabled = true;
+      }
+      btnEl.disabled = false;
       userSelectedDate = selectedDates[0];
       console.log(userSelectedDate);
     },
   };
 
   flatpickr(inputEl, options);
+
+  function handleClick() {
+    //console.log(userSelectedDate.getDate);
+  };
+
+  btnEl.addEventListener("click", handleClick);
+
+
 
