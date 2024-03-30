737a9bc0391a3898e03217a0283d7c8c44f488e8
HannaTkachenko2204 <anna.kuvakina91@gmail.com>
2024-03-30 21:53:45 +0000

    Deploying to gh-pages from @ HannaTkachenko2204/goit-js-hw-10@482cab034f4e95d28a40ae2c82820cc9478cd6d9 🚀

--- assets/1-timer-9c10bc65.js
+++
@@ -1,2 +0,0 @@
-import{f as t}from"./vendor-2b44ac2e.js";console.log("Timer");const n=document.querySelector("#datetime-picker");let o;const l={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0]),o=e[0],console.log(o)}};t(n,l);
-//# sourceMappingURL=1-timer-9c10bc65.js.map
