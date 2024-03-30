482cab034f4e95d28a40ae2c82820cc9478cd6d9
Анна <anna.kuvakina91@gmail.com>
2024-03-30 23:53:30 +0200

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,15 +1,38 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-import { createLogger } from "vite";
-
-console.log("Timer");
 
 const inputEl = document.querySelector("#datetime-picker");
 const btnEl = document.querySelector("button");
+const daysEl = document.querySelector("[data-days]")
+const hoursEl = document.querySelector("[data-hours]");
+const minutesEl = document.querySelector("[data-minutes]");
+const secondsEl = document.querySelector("[data-seconds]");
 
 btnEl.disabled = true;
-
 let userSelectedDate;
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, "0");
+}
 
 const options = {
     enableTime: true, // boolean false Включает выбор времени
@@ -21,6 +44,7 @@ const options = {
       if(selectedDates[0].getTime() < options.defaultDate.getTime()) {
         window.alert("Please choose a date in the future");
         btnEl.disabled = true;
+        return;
       }
       btnEl.disabled = false;
       userSelectedDate = selectedDates[0];
@@ -31,10 +55,22 @@ const options = {
   flatpickr(inputEl, options);
 
   function handleClick() {
-    //console.log(userSelectedDate.getDate);
+    btnEl.disabled = true;
+    inputEl.disabled = true;
+    // const resalt = userSelectedDate.getTime() - Date.now();
+    // console.log(resalt);
+    // daysEl.textContent = addLeadingZero(convertMs(resalt).days);
+    // hoursEl.textContent = addLeadingZero(convertMs(resalt).hours);
+    // minutesEl.textContent = addLeadingZero(convertMs(resalt).minutes);
+    // secondsEl.textContent = addLeadingZero(convertMs(resalt).seconds);
+    setInterval(() => {
+      const resalt = userSelectedDate.getTime() - Date.now();
+      console.log(resalt);
+      daysEl.textContent = addLeadingZero(convertMs(resalt).days);
+      hoursEl.textContent = addLeadingZero(convertMs(resalt).hours);
+      minutesEl.textContent = addLeadingZero(convertMs(resalt).minutes);
+      secondsEl.textContent = addLeadingZero(convertMs(resalt).seconds);
+    }, 1000)
   };
 
   btnEl.addEventListener("click", handleClick);
-
-
-
